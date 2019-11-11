306ccc99d3b3697e71b75d5c316e4707c93458b7
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-11-11 09:36:23 -0800

    not done

--- api/api-router.js
+++ api/api-router.js
@@ -1,13 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.json({ api: "It's alive" });
+});
+
+router.post("/hash", (req, res) => {
+  const passWord = req.body.password;
 });
 
 module.exports = router;
